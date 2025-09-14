1769b0a66e742a112989f5540902693008a56c96
Agrim <agrimgupta31@gmail.com>
2025-09-14 16:21:14 +0530

    Inititial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import dagshub
+
+
+n_estimators = 10
+max_depth = 5
+
+# Load dataset
+data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+
+mlflow.set_experiment("Iris_Classification_Experiment")
+
+with mlflow.start_run() as run:
+    # Initialize and train model
+    model = RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth, random_state=42)
+    model.fit(X_train, y_train)
+
+    # Make predictions
+    predictions = model.predict(X_test)
+
+    # Evaluate model
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Model Accuracy: {accuracy}")
+
+    # log parameters
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+
+    # Log model and metrics to MLflow
+    
+    # mlflow.sklearn.log_model(model, "random_forest_model")
+    mlflow.log_metric("accuracy", accuracy)
+
+    run_id = run.info.run_id
+    print(f"Run ID: {run_id}")
